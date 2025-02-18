2e189de39470289fda673f1c77c43ef757412953
Joe Polny <joepolny@gmail.com>
2025-02-17 19:58:13 -0500

    use markdownlint

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -39,6 +39,7 @@ return {
         -- Conform can also run multiple formatters sequentially
         python = { 'black' },
         typescript = { 'prettier' },
+        markdown = { 'markdownlint' },
         --
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
