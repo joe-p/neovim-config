bed23132dad2964cf84f4f296eb4342b2b78c8ad
Joe Polny <joepolny@gmail.com>
2025-03-02 08:39:21 -0500

    add toggle keys for conform

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -12,10 +12,28 @@ return {
         mode = '',
         desc = '[F]ormat buffer',
       },
+      {
+        '<leader>tf',
+        function()
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+        end,
+        desc = 'Toggle [F]ormat on save',
+      },
+      {
+        '<leader>tF',
+        function()
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
+        end,
+        desc = 'Toggle [F]ormat on save for this buffer',
+      },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
