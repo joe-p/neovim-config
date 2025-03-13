0a37bdfb10005aa49742cc292cf86e2d1b8ed2f7
Joe Polny <joepolny@gmail.com>
2025-03-13 09:23:29 -0400

    diffview diffopt

--- lua/joe-p/plugins/diffview.lua
+++ lua/joe-p/plugins/diffview.lua
@@ -1,5 +1,19 @@
 return {
   'sindrets/diffview.nvim',
+  config = function()
+    require('diffview').setup {}
+
+    -- From https://www.reddit.com/r/neovim/s/Ibfxg6As5b
+    vim.opt.diffopt = {
+      'internal',
+      'filler',
+      'closeoff',
+      'context:12',
+      'algorithm:histogram',
+      'linematch:200',
+      'indent-heuristic',
+    }
+  end,
   keys = {
     {
       '<leader>gd',
