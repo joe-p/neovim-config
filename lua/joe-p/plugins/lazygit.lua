57cb461801399d6c8bebf9b30797eeb4dfe8fcd7
Joe Polny <joepolny@gmail.com>
2025-03-17 20:47:25 -0400

    rm bufhidden=wipe

--- lua/joe-p/plugins/lazygit.lua
+++ lua/joe-p/plugins/lazygit.lua
@@ -10,7 +10,7 @@ return {
   },
   config = function()
     vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
-    vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
+    vim.env.GIT_EDITOR = 'nvr -cc split --remote-wait'
     vim.g.lazygit_use_custom_config_file_path = 1 -- config file path is evaluated if this value is 1
     vim.g.lazygit_config_file_path = vim.fn.expand '$HOME/.config/lazygit/config.yml'
   end,
