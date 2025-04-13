f7d6079b8d94275c97a3495f5eb5fc29e95536de
Joe Polny <joepolny@gmail.com>
2025-04-13 08:09:46 -0400

    remove buffer from blink

--- lua/joe-p/plugins/blink.lua
+++ lua/joe-p/plugins/blink.lua
@@ -37,7 +37,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'buffer' },
+      default = { 'lsp', 'path' },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
