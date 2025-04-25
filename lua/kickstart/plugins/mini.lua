896cf8d26c91a3f7b4a6a45b7b5a384499f0bd90
Joe Polny <joepolny@gmail.com>
2025-04-25 09:21:23 -0400

    use x for mini surround

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -15,7 +15,20 @@ return {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      -- require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'xa', -- Add surrounding in Normal and Visual modes
+          delete = 'xd', -- Delete surrounding
+          find = 'xf', -- Find surrounding (to the right)
+          find_left = 'xF', -- Find surrounding (to the left)
+          highlight = 'xh', -- Highlight surrounding
+          replace = 'xr', -- Replace surrounding
+          update_n_lines = 'xn', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
