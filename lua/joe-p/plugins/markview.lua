14a992724d74af6ce1efcfcd9428e2bd496b68d6
Joe Polny <joepolny@gmail.com>
2025-05-20 08:07:37 -0400

    replace copilot with goose

--- lua/joe-p/plugins/markview.lua
+++
@@ -1,8 +0,0 @@
--- For `plugins/markview.lua` users.
-return {
-  'OXY2DEV/markview.nvim',
-  lazy = false,
-  keys = {
-    { '<leader>Tm', '<cmd>Markview Toggle<cr>', desc = 'Toggle [M]arkview' },
-  },
-}
