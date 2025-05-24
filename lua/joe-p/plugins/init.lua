af0efce872d726a73730d2ddf9f3ab8278046a2c
Joe Polny <joepolny@gmail.com>
2025-05-23 21:04:35 -0400

    neovim dir for vscode support

--- lua/joe-p/plugins/init.lua
+++ lua/joe-p/plugins/init.lua
@@ -2,6 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  'nvim-treesitter/nvim-treesitter-context',
-}
+
+if not vim.g.vscode then
+  return { import = 'joe-p.plugins.neovim' }
+end
+
+return {}
