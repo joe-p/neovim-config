c90785094ce0e4b4aa18dc2107ce27fb4b90aeaf
Joe Polny <joepolny@gmail.com>
2025-05-27 06:56:46 -0400

    vscode quickopen and select project

--- lua/joe-p/keymaps.lua
+++ lua/joe-p/keymaps.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-global
 vim.keymap.del('n', 'gri')
 vim.keymap.del('n', 'grr')
 vim.keymap.del('x', 'gra')
@@ -22,7 +23,7 @@ end, { desc = 'Previous [H]int' })
 
 if vim.g.vscode then
   local vscode = require 'vscode'
-  
+
   vim.keymap.set('n', 'gr', function()
     vim.lsp.buf.references()
   end)
@@ -51,7 +52,65 @@ if vim.g.vscode then
   end)
 
   -- rename symbol
-  vim.keymap.set('n', '<leader>rn', function()
+  vim.keymap.set('n', '<leader>cr', function()
     vscode.call('editor.action.rename')
   end)
+
+  -- workbench.action.quickOpen
+  vim.keymap.set('n', '<leader>ff', function()
+    vscode.call('workbench.action.quickOpen')
+  end)
+
+  local function select_workspace()
+    local projects = {
+      { path = os.getenv("HOME") .. "/.config/nvim", name = "nvim" },
+    }
+
+    local home = vim.env.HOME
+    local git_dir = home .. "/git"
+    local cmd = string.format(
+      "/opt/homebrew/bin/fd --hidden --no-ignore ^.git$ --max-depth=3 --prune %s",
+      git_dir
+    )
+
+    local output = vim.fn.system(cmd)
+
+    -- Check if command was successful
+    if vim.v.shell_error ~= 0 then
+      vim.notify("Failed to run fd: " .. output, vim.log.levels.ERROR)
+      return projects
+    end
+
+    -- Process each line of output
+    for line in output:gmatch("([^\n]*)\n?") do
+      if line ~= "" then
+        local project = line:gsub("/.git.*$", "")
+        local label = project
+        local id = project:gsub(".*/", "")
+        table.insert(projects, { path = tostring(label), name = tostring(id) })
+      end
+    end
+
+    local prompt = "Select a repo"
+
+    vim.ui.select(projects, {
+      prompt = prompt,
+      format_item = function(project)
+        return project.name
+      end,
+    }, function(choice)
+      if choice then
+        vscode.eval([[
+        vscode.commands.executeCommand(
+          'vscode.openFolder',
+          vscode.Uri.file(']] .. choice.path .. [['),
+          { forceNewWindow: true }
+        );
+        ]])
+
+      end
+    end)
+  end
+
+  vim.keymap.set('n', '<leader>ww', select_workspace)
 end
