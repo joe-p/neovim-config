de55524c3e3d4103a138c973c22577f839736c70
Joe Polny <joepolny@gmail.com>
2025-09-04 10:48:49 -0400

    use git branch name in session name

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -8,6 +8,8 @@ return {
   ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    git_use_branch_name = true, -- Include git branch name in session name
+    git_auto_restore_on_branch_change = true, -- Should we auto-restore the session when the git branch changes. Requires git_use_branch_name
     -- log_level = 'debug',
     pre_save_cmds = {
       function()
