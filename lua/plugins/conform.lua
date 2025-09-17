b0f1c3cadf80b4851b9edd41ac872e7b0485befd
Joe Polny <joepolny@gmail.com>
2025-09-17 08:03:35 -0400

    toggle formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,13 +11,35 @@ return {
       json = { 'prettier' },
       c = { 'clang-format' },
     },
-    format_on_save = {
-      filter = function(client)
-        return client.name ~= 'ts_ls'
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return {
+        filter = function(client)
+          return client.name ~= 'ts_ls'
+        end,
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      }
+    end,
+  },
+  keys = {
+    {
+      '<leader>Tf',
+      function()
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
       end,
-      -- These options will be passed to conform.format()
-      timeout_ms = 500,
-      lsp_format = 'fallback',
+      desc = 'Toggle [F]ormat on save',
+    },
+    {
+      '<leader>TF',
+      function()
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+      end,
+      desc = 'Toggle [F]ormat on save for this buffer',
     },
   },
 }
