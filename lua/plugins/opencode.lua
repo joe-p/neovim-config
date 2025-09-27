4531e27df9676710e4256b1c2013145d8070b014
Joe Polny <joepolny@gmail.com>
2025-09-27 07:36:03 -0400

    add sudo-tee/opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,64 +1,17 @@
 return {
-  'NickvanDyke/opencode.nvim',
+  'sudo-tee/opencode.nvim',
+  config = function()
+    require('opencode').setup {}
+  end,
   dependencies = {
-    -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal — otherwise optional
-    { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
-  },
-  ---@type opencode.Opts
-  opts = {
-    terminal = {
-      win = {
-        enter = true,
-        position = 'float',
+    'nvim-lua/plenary.nvim',
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        anti_conceal = { enabled = false },
+        file_types = { 'markdown', 'opencode_output' },
       },
-    },
-  },
-  keys = {
-    {
-      '<leader>aa',
-      function()
-        local opencode = require 'opencode'
-        opencode.command 'session_new'
-        opencode.ask '@cursor: '
-      end,
-      desc = 'Ask opencode about this',
-      mode = 'n',
-    },
-    {
-      '<leader>aa',
-      function()
-        local opencode = require 'opencode'
-
-        opencode.command 'session_new'
-        opencode.ask '@selection: '
-      end,
-      desc = 'Ask opencode about selection',
-      mode = 'v',
-    },
-    {
-      '<leader>ac',
-      function()
-        require('opencode').toggle()
-      end,
-      desc = 'Toggle embedded opencode',
-    },
-    {
-      '<leader>an',
-      function()
-        require('opencode').command 'session_new'
-      end,
-      desc = 'New session',
-    },
-    -- Example: keymap for custom prompt
-    {
-      '<leader>ad',
-      function()
-        local opencode = require 'opencode'
-
-        opencode.command 'session_new'
-        opencode.ask 'Explain @diagnostic and how to fix it'
-      end,
-      desc = 'Explain code near cursor',
+      ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
     },
   },
 }
