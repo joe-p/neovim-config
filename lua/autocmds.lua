a0d86f323c4d3ba8dd84f23f38367baed0d1217b
Joe Polny <joepolny@gmail.com>
2025-10-24 20:54:49 -0400

    mdx as markdown

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -13,3 +13,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.mdx',
+  callback = function()
+    vim.bo.filetype = 'markdown'
+  end,
+})
