fd6da03b07be983468417a86845810950096137a
Joe Polny <joepolny@gmail.com>
2025-11-22 07:10:05 -0500

    swap llama keys

--- lua/plugins/llama.lua
+++ lua/plugins/llama.lua
@@ -2,8 +2,8 @@ return {
   'ggml-org/llama.vim',
   init = function()
     vim.g.llama_config = {
-      keymap_accept_full = '<C-j>',
-      keymap_accept_line = '<C-l>',
+      keymap_accept_full = '<C-l>',
+      keymap_accept_line = '<C-k>',
       show_info = 0,
     }
   end,
