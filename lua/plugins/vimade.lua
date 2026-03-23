1dcbd7f5d6d5bd6fabcc28743f38a68bdcc2f45c
Joe Polny <joepolny@gmail.com>
2026-03-23 08:33:05 -0400

    link opencode windows for vimade

--- lua/plugins/vimade.lua
+++ lua/plugins/vimade.lua
@@ -3,6 +3,14 @@ return {
     'tadaa/vimade',
     enabled = true,
     opts = {
+      link = {
+        opencode = function(win, active)
+          local win_ft = win.buf_opts.filetype
+          local active_ft = active.buf_opts.filetype
+
+          return win_ft == ('opencode' and active_ft == 'opencode_output') or (win_ft == 'opencode_output' and active_ft == 'opencode')
+        end,
+      },
       recipe = { 'default', { animate = false } },
       ncmode = 'windows',
       fadelevel = 0.6,
