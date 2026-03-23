6814a8f00bb5f5a7f56ce1e505e19cd0195a8638
Joe Polny <joepolny@gmail.com>
2026-03-23 07:26:35 -0400

    fim_trigger to avoid <space> keymap in insert

--- lua/plugins/llama.lua
+++ lua/plugins/llama.lua
@@ -1,5 +1,6 @@
 -- llama-server --fim-qwen-7b-default
 -- llama-server -hf sweepai/sweep-next-edit-1.5B --port 8012
+-- llama-server --hf-file zeta-Q8_0.gguf -hf lmstudio-community/zeta-GGUF --port 8012
 return {
   'ggml-org/llama.vim',
   enabled = true,
@@ -7,6 +8,7 @@ return {
     vim.g.llama_config = {
       keymap_fim_accept_full = '<C-l>',
       keymap_fim_accept_line = '<C-k>',
+      keymap_fim_trigger = '<C-j>',
       show_info = 0,
     }
   end,
