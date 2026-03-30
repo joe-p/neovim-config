32708dde8c6c462205092d8c9b1dabdfce2f4aa6
Joe Polny <joepolny@gmail.com>
2026-03-30 10:21:44 -0400

    move comment

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,8 @@
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '-'
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.del('n', 'gri')
