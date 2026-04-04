9d4828ea0b229d526219200793b6c9a54021fe13
Joe Polny <joepolny@gmail.com>
2026-04-04 07:56:07 -0400

    codesnap config

--- plugin/codesnap.lua
+++ plugin/codesnap.lua
@@ -1,5 +1,21 @@
 -- See https://github.com/mistricky/codesnap.nvim/issues/162#issuecomment-4166538711
 vim.defer_fn(function()
   vim.pack.add { 'https://github.com/mistricky/codesnap.nvim' }
-  require('codesnap').setup {}
-end, 50)
+  require('codesnap').setup {
+    background = '#00000000',
+    snapshot_config = {
+      window = {
+        margin = {
+          x = 0,
+          y = 0,
+        },
+      },
+      code_config = {
+        font_family = 'MesloLGM Nerd Font Mono',
+        breadcrumbs = {
+          enable = false,
+        },
+      },
+    },
+  }
+end, 100)
