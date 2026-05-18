7d9e39de15cdd07a0fa9eeb0652eca000e0230bc
Joe Polny <joepolny@gmail.com>
2026-05-18 11:03:19 -0400

    code diags only

--- plugin/trouble.lua
+++ plugin/trouble.lua
@@ -1,5 +1,27 @@
-vim.pack.add({ 'https://github.com/folke/trouble.nvim' })
-require('trouble').setup()
+vim.pack.add { 'https://github.com/folke/trouble.nvim' }
 
-vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle focus=true<cr>', { desc = 'Diagnostics (Trouble)' })
-vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<cr>', { desc = 'Buffer Diagnostics (Trouble)' })
+require('trouble').setup {
+  modes = {
+    code_diags = {
+      mode = 'diagnostics',
+      focus = true,
+      filter = function(items)
+        return vim.tbl_filter(function(item)
+          return item.item.source ~= 'Harper'
+        end, items)
+      end,
+    },
+    buf_code_diags = {
+      mode = 'diagnostics',
+      focus = true,
+      filter = function(items)
+        return vim.tbl_filter(function(item)
+          return item.buf == 0 and item.item.source ~= 'Harper'
+        end, items)
+      end,
+    },
+  },
+}
+
+vim.keymap.set('n', '<leader>xX', '<cmd>Trouble code_diags<cr>', { desc = 'Diagnostics (Trouble)' })
+vim.keymap.set('n', '<leader>xx', '<cmd>Trouble buf_code_diags<cr>', { desc = 'Buffer Diagnostics (Trouble)' })
